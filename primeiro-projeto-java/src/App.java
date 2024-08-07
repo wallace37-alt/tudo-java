fd063eaf58fd7ca20989346a37883dc34cfff3b4
wallace das virgens santo <wallacebinho2@gmail.com>
2024-08-07 19:42:47 -0300

    aula nova

--- primeiro-projeto-java/src/App.java
+++ primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("vamos,nessa");
+        System.out.println("vamos,nessa, alera iniciar o curso kkk");
     }
 }
